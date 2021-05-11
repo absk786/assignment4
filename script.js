dd3b8e960741c4c8c06832eb8ef1525e258febd5
Abdul Rehman Khalil <abdulrehmankhalil@Abduls-MacBook-Air.local>
2021-05-10 21:06:45 -0400

    basic layout and plan of project

---
+++ script.js
@@ -0,0 +1,10 @@
+// function to start the game and timer
+// function to stop and reset the game and timer
+// function which determines the right answer
+// functoin which determines the points logic
+// function which displays the next question
+// function which takes the questiom data and populates it and stores the information
+// function to store the initials
+// function to keep the high scores
+// function to submit the scores
+// function to clear reset the score table
