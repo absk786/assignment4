ad50ea3fc21e9f0b19a60ac8db9a12130e0d4ab0
Abdul Rehman Khalil <abdulrehmankhalil@Abduls-MacBook-Air.local>
2021-05-15 10:49:40 -0400

    asdf'

--- script.js
+++ script.js
@@ -1,105 +1,125 @@
-// function to start the game and timer
-// Function handleTimer - timer logic 
+
 // function to stop and reset the game and timer
-// functoin which determines the points logic
-// function which displays the next question
 // function which determines the right answer
-// function which takes the questiom data and populates it and stores the information
 // function to store the initials function to keep the high scores function to submit the scores
 // function to clear reset the score table
 
-let startGameTimeEl = 100;
 let finishGameTimeEl = 0;
-// let timeIntervalEl = (currentTime(),1000); 
+let currentTimeEl = document.getElementById('currentTimeEl')
+let startGame =document.getElementById('startBtn');
 let answerContainer = document.querySelector('.answerSection');
+let questionContainer = document.querySelector('.questionSection');
 let questionDisplay = document.querySelector('.questionStyle');
-let nextQuestoin = document.querySelector('.nextButton');
-
-var questionData = [
-{
-    title:"question1", 
-    choices:["q1a1",'q1b1','q1c1','q1d1'],
-    answer:'a'
-},
-{
-    title:"question2", 
-    choices:["q2a2",'q2b2','q2c2','q2d2'],
-    answer:'a'
-},{
-    title:"question3", 
-    choices:["q3a3",'q3b3','q3c3','q3d3'],
-    answer:'a'
-},{
-    title:"question4", 
-    choices:["q4a4",'q4b4','q4c4','q4d4'],
-    answer:'a'
-},
+let questionNumber = 0;
+let nextQuestionBtn = document.getElementById('nextQuestion');
+let questionData = [
+    {
+        title:"This is question1", 
+        choices:["question 1 answer 1",'question 1 answer 2','question 1 answer 3','question 1 answer 4'],
+        answer:'a'
+    },
+    {
+        title:"this is question2", 
+        choices:["question 2 answer 1",'question 2 answer 2','question 2 answer 3','question 2 answer 4'],
+        answer:'a'
+    },{
+        title:"this is question3", 
+        choices:["question 3 answer 1",'question 3 answer 2','question 3 answer 3','question 3 answer 4'],
+        answer:'a'
+    },{
+        title:"this is question4", 
+        choices:["question 4 answer 1",'question 4 answer 2','question 4 answer 3','question 4 answer 4'],
+        answer:'a'
+    },
 ]
+let correctAns1 = questionData[0].choices[1];
+let correctAns1 = questionData[2].choices[3];
+let correctAns1 = questionData[3].choices[0];
+let correctAns1 = questionData[4].choices[1];
+
+// this function starts the game time and displays it 
+let timeHandler = function () {
+    let startGameTimeEl = 100;
+    let timeInterval = setInterval(function () {
+    if (startGameTimeEl > 1) {
+        currentTimeEl.textContent = startGameTimeEl + ' seconds remaining';
+        startGameTimeEl--;
+    }
+    else if (startGameTimeEl === 1) {
+        currentTimeEl.textContent = startGameTimeEl + ' second remaining';
+        startGameTimeEl--;
+    }
+    else {
+        currentTimeEl.textContent == '';
+        clearInterval(timeInterval);
+        window.alert("game over");
+    }
+        }, 1000);
+}
+
 // this function will create the answer buttons on the page
-let answerButtonHandler = function (btnID) {
-    question = document.createElement('title');
+ function answerButtonHandler (btnID) {
+
+    question = document.createElement('p');
     question.className ='questionStyle';
-    question.textContent = questionData[0].title;
-    
+    question.textContent = questionData[questionNumber].title;
+    console.log(questionData[questionNumber].title);
+    questionContainer.appendChild(question);
+
     answerBtn1 = document.createElement('button');
     answerBtn1.className = 'answerBtn';
     answerBtn1.setAttribute = ('btn-id', btnID);
-    answerBtn1.textContent = questionData[0].choices[0];
+    answerBtn1.textContent = questionData[questionNumber].choices[0];
     answerContainer.appendChild(answerBtn1);
 
     answerBtn2 = document.createElement('button');
     answerBtn2.className = 'answerBtn';
     answerBtn2.setAttribute = ('btn-id', btnID);
-    answerBtn2.textContent = questionData[0].choices[1];
+    answerBtn2.textContent = questionData[questionNumber].choices[1];
     answerContainer.appendChild(answerBtn2);
 
     answerBtn3 = document.createElement('button');
     answerBtn3.className = 'answerBtn';
     answerBtn3.setAttribute = ('btn-id', btnID);
-    answerBtn3.textContent = questionData[0].choices[2];
+    answerBtn3.textContent = questionData[questionNumber].choices[2];
     answerContainer.appendChild(answerBtn3);
 
     answerBtn4 = document.createElement('button');
     answerBtn4.className = 'answerBtn';
     answerBtn4.setAttribute = ('btn-id', btnID);
-    answerBtn4.textContent = questionData[0].choices[3];
+    answerBtn4.textContent = questionData[questionNumber].choices[3];
     answerContainer.appendChild(answerBtn4);
 }
-answerButtonHandler ();
 
-// event listnerers
-mainArea.eventListerners('click', questionPasser);
-
-
-let oldQuestion;
-let newQuestion;
-
-let questionPasser = function () {
-   
-    for (q=0; q=4; q++) {
-        
+// this functoin will generate the next question
+function nextQuestion () {
+    console.log('the next question button is working');
+    answerButtonHandler(questionNumber);
+    questionNumber='';
+    questionNumber ++;
+    if (questionNumber > questionData.length) {
+        window.alert("the game is now finished");
+        questionNumber ='';
     }
+}
+
+// this function will determine the points logic
+let playerAnswer = '';
+
+function determinePoints () {
+
+    if (playerAnswer === ) {
+
+    }
+
 
 }
 
-questionPasser();
 
-// let t;
-// for (t=0; t<startGameTimeEl;t--);
 
-// let currentTime = function()  {
-//     startGameTimeEl--;
-//     console.log(currentTime);
-// }
 
-// let currentTime = function (){
-
-// }  
-
-//     startGameTimeEl -=1 = currentTimeEl;
-
-//     console.log(currentTimeEl);
-// }
+startGame.onclick = timeHandler;
+nextQuestionBtn.onclick = nextQuestion;
 
 
 // var index = 0;
